b3973c0c8132b89715329a0f08d9648ff183cf3c
Ross Golder <ross@golder.org>
2026-02-17 21:11:07 +0700

    cleanup: Remove 5 defunct exchange providers
    Remove providers for exchanges that are no longer operational:
    - FTX (collapsed Nov 2022, domain seized)
    - Bittrex (shut down Dec 2023)
    - Poloniex (API unreachable after hack)
    - TomoX/TomoChain DEX (shut down)
    - VccExchange (defunct)

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -11,14 +11,12 @@ import * as ProviderBitPay from './ProviderBitPay';
 import * as ProviderBitkub from './ProviderBitkub';
 import * as ProviderBitso from './ProviderBitso';
 import * as ProviderBitstamp from './ProviderBitstamp';
-import * as ProviderBittrex from './ProviderBittrex';
 import * as ProviderBuda from './ProviderBuda';
 import * as ProviderBTCMarkets from './ProviderBTCMarkets';
 import * as ProviderCexio from './ProviderCexio';
 import * as ProviderCoinbase from './ProviderCoinbase';
 import * as ProviderCoinGecko from './ProviderCoinGecko';
 import * as ProviderCryptoCompare from './ProviderCryptoCompare';
-import * as ProviderFtx from './ProviderFtx';
 import * as ProviderGate from './ProviderGate';
 import * as ProviderHitBTC from './ProviderHitBTC';
 import * as ProviderHuobi from './ProviderHuobi';
@@ -27,9 +25,6 @@ import * as ProviderKucoin from './ProviderKucoin';
 import * as ProviderMEXC from './ProviderMEXC';
 import * as ProviderNobitex from './ProviderNobitex';
 import * as ProviderPaymium from './ProviderPaymium';
-import * as ProviderPoloniex from './ProviderPoloniex';
-import * as ProviderTomoX from './ProviderTomoX';
-import * as ProviderVccExchange from './ProviderVccExchange';
 import * as ProviderBybit from './ProviderBybit';
 import * as ProviderBybitPerpetual from './ProviderBybitPerpetual';
 
@@ -43,7 +38,6 @@ export const Providers: Record<string, BaseProvider.Api> = {
   bitkub: new ProviderBitkub.Api(),
   bitso: new ProviderBitso.Api(),
   bitstamp: new ProviderBitstamp.Api(),
-  bittrex: new ProviderBittrex.Api(),
   btcmarkets: new ProviderBTCMarkets.Api(),
   buda: new ProviderBuda.Api(),
   bybit: new ProviderBybit.Api(),
@@ -52,7 +46,6 @@ export const Providers: Record<string, BaseProvider.Api> = {
   coinbase: new ProviderCoinbase.Api(),
   coingecko: new ProviderCoinGecko.Api(),
   cryptocompare: new ProviderCryptoCompare.Api(),
-  ftx: new ProviderFtx.Api(),
   gate: new ProviderGate.Api(),
   hitbtc: new ProviderHitBTC.Api(),
   huobi: new ProviderHuobi.Api(),
@@ -61,9 +54,6 @@ export const Providers: Record<string, BaseProvider.Api> = {
   mexc: new ProviderMEXC.Api(),
   nobitex: new ProviderNobitex.Api(),
   paymium: new ProviderPaymium.Api(),
-  poloniex: new ProviderPoloniex.Api(),
-  tomox: new ProviderTomoX.Api(),
-  vccexchange: new ProviderVccExchange.Api(),
 };
 
 export type ProviderKey = keyof typeof Providers;
