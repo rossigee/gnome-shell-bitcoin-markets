de8b1f3d64f3a307f668cce74b1d601b4a35a5e1
Ross Golder <ross@golder.org>
2026-02-18 11:08:23 +0700

    fix: uppercase only symbol in Bitkub URL, not path
    The path /api/market/ticker is case-sensitive, so uppercasing the
    entire URL caused 404 errors.

--- src/providers/ProviderBitkub.ts
+++ src/providers/ProviderBitkub.ts
@@ -3,12 +3,15 @@ import * as BaseProvider from './BaseProvider';
 export class Api extends BaseProvider.Api {
   apiName = 'Bitkub';
 
-  apiDocs = [['API Docs', 'https://github.com/bitkub/bitkub-official-api-docs/blob/master/restful-api.md#get-apimarketbids']];
+  apiDocs = [
+    ['API Docs', 'https://github.com/bitkub/bitkub-official-api-docs/blob/master/restful-api.md#get-apimarketbids'],
+  ];
 
   interval = 60; // unclear, should be safe
 
   getUrl({ base, quote }) {
-    return `https://api.bitkub.com/api/market/ticker?sym=${quote}_${base}`.toUpperCase();
+    const symbol = `${quote}_${base}`.toUpperCase();
+    return `https://api.bitkub.com/api/market/ticker?sym=${symbol}`;
   }
 
   getLast(data, { base, quote }) {
